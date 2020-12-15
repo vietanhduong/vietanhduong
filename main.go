52ca884ff21958bd00575105ca3a1210ff2773f4
vietanhduong <vietanhs0817@gmail.com>
2020-12-15 18:09:44 +0700

    Add main.go

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+	res, err := http.Get("https://anhdv.dev/api/README.md")
+	if err != nil {
+		os.Exit(1)
+	}
+	f, err := os.Create("./README.md")
+	if err != nil {
+		os.Exit(1)
+	}
+	defer f.Close()
+	bytes, _ := ioutil.ReadAll(res.Body)
+	f.Write(bytes)
+}
