d13a7d1d23429bcca50d7cc888f5295513a2023e
vietanhduong <vietanhs0817@gmail.com>
2021-06-15 10:06:02 +0700

    provide install script

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# install brew first
+curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash 
+
+# install what I need with brew
+./brew/dumps.sh
+
+
+# install oh-my-zsh
+curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | bash
+
+# set default zsh
+sudo sh -c "echo $(which zsh) >> /etc/shells" && chsh -s $(which zsh)
+
+# link config
+ln -s .tmux.conf ~/.tmux.conf
+ln -s .vimrc ~/.vimrc
+
+# make sure .zshrc already removed
+rm -f ~/.zshrc
+ln -s .zshrc ~/.zshrc
