e36f4cab53a8e8f728a8d0d98dc1eb69a41e44af
Viet Anh Duong <vietanhs0817@gmail.com>
2021-06-16 17:12:07 +0700

    update install script

--- install.sh
+++ install.sh
@@ -14,8 +14,8 @@ curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools
 sudo sh -c "echo $(which zsh) >> /etc/shells" && chsh -s $(which zsh)
 
 # link config
-ln -s ./.tmux.conf ~/.tmux.conf
-ln -s ./.vimrc ~/.vimrc
+ln -s .tmux.conf /Users/${USER}/.tmux.conf
+ln -s .vimrc /Users/${USER}/.vimrc
 
 # install plugins for zsh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
