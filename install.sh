ab79c4f27935210fa036b17c286762e07f6f39a1
Viet Anh Duong <vietanhs0817@gmail.com>
2021-06-16 10:32:40 +0700

    remove junks

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # install brew packages
-xargs brew install < ./brew/leaves
-xargs brew install --cask < ./brew/casks
+xargs brew install <./brew/leaves
+xargs brew install --cask <./brew/casks
 
 # install Hack fonts
 cp -r ./fonts/hack/* ~/Library/fonts
@@ -19,5 +19,3 @@ ln -s ./.vimrc ~/.vimrc
 
 # install plugins for zsh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-## comment
