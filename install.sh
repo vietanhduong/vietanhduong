6911401d45cbf1bdd7914c5f4179639336462496
Viet Anh Duong <vietanhs0817@gmail.com>
2021-11-09 17:55:51 +0700

    Update config

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ xargs brew install <./brew/leaves
 xargs brew install --cask <./brew/casks
 
 # install font
-APPLY_FONT=roboto-mono
+APPLY_FONT=hack
 cp -r ./fonts/$APPLY_FONT/* ~/Library/fonts
 
 # install oh-my-zsh
