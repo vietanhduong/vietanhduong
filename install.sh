e640a6512c6a79337a26ed3bad7a19c42869e34c
Viet-Anh Duong <vietanhs0817@gmail.com>
2021-11-14 12:24:37 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ xargs brew install <./brew/leaves
 xargs brew install --cask <./brew/casks
 
 # install font
-APPLY_FONT=consolas
+APPLY_FONT=monolisa
 cp -r ./fonts/$APPLY_FONT/* ~/Library/fonts
 
 # install oh-my-zsh
