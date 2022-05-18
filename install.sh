2e8e862b7b277417b49e358b2abc76f8ab82098c
Viet-Anh Duong <vietanhs0817@gmail.com>
2022-05-18 19:50:37 +0700

    change fonts
    Signed-off-by: Viet-Anh Duong <vietanhs0817@gmail.com>

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ xargs brew install <./brew/leaves
 xargs brew install --cask <./brew/casks
 
 # install font
-APPLY_FONT=meslo-lg
+APPLY_FONT=source-code-pro
 cp -r ./fonts/$APPLY_FONT/* ~/Library/fonts
 
 # install oh-my-zsh
