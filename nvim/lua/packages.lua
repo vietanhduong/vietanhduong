96bd9bbf60a4fc504690ec537d815ccc5abb475c
Viet Anh Duong <vietanhs0817@gmail.com>
2024-01-01 10:30:14 +0700

    Remove old vim config, switch to lua
    Signed-off-by: Viet Anh Duong <vietanhs0817@gmail.com>

---
+++ nvim/lua/packages.lua
@@ -0,0 +1,25 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    print("Installed packer!")
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+
+  use 'wellle/context.vim'
+  use 'chaoren/vim-wordmotion'
+  use 'nvim-treesitter/nvim-treesitter'
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
