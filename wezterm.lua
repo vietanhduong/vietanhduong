4704c0dfc1096c2437b2c5ab54d7d20a77564d04
Viet Anh Duong <vietanhs0817@gmail.com>
2024-06-16 13:33:44 +0700

    Update config
    Signed-off-by: Viet Anh Duong <vietanhs0817@gmail.com>

--- wezterm.lua
+++ wezterm.lua
@@ -4,11 +4,16 @@ local config = wezterm.config_builder()
 
 config.font = wezterm.font 'SF Mono'
 config.font_size = 14.0
+
+config.color_scheme = 'Gruvbox Dark (Gogh)'
+
 config.window_padding = {
   left = '1cell',
   right = '1cell',
   top = '0.5cell',
   bottom = '0.5cell',
 }
+
+config.use_fancy_tab_bar = false
 
 return config
