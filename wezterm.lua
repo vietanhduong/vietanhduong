b826a68362c794e1725b98d46582dfbce0274972
Viet Anh Duong <vietanhs0817@gmail.com>
2024-06-16 13:20:46 +0700

    Update config
    Signed-off-by: Viet Anh Duong <vietanhs0817@gmail.com>

--- wezterm.lua
+++ wezterm.lua
@@ -4,5 +4,11 @@ local config = wezterm.config_builder()
 
 config.font = wezterm.font 'SF Mono'
 config.font_size = 14.0
+config.window_padding = {
+  left = '1cell',
+  right = '1cell',
+  top = '0.5cell',
+  bottom = '0.5cell',
+}
 
 return config
