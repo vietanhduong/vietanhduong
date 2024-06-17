f7e490a31f52992a7754b861faed9b7a2138cf7f
Viet Anh Duong <vietanhs0817@gmail.com>
2024-06-17 11:06:10 +0700

    Update config
    Signed-off-by: Viet Anh Duong <vietanhs0817@gmail.com>

--- wezterm.lua
+++ wezterm.lua
@@ -15,5 +15,12 @@ config.window_padding = {
 }
 
 config.use_fancy_tab_bar = false
+config.keys = {
+  -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+  {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
+  -- Make Option-Right equivalent to Alt-f; forward-word
+  {key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
+}
 
+config.window_background_opacity = 0.87
 return config
