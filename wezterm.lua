887b74c5cb8708882bac8b26ac742691493d8995
Viet Anh Duong <vietanhs0817@gmail.com>
2024-06-21 10:42:25 +0700

    Update config
    Signed-off-by: Viet Anh Duong <vietanhs0817@gmail.com>

--- wezterm.lua
+++ wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.font = wezterm.font 'Menlo'
+config.font = wezterm.font 'Jetbrains Mono'
 config.font_size = 14.0
 
 config.color_scheme = 'Wombat'
@@ -11,4 +11,12 @@ config.keys = {
   {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
   {key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
 }
+
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
 return config
