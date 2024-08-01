c21394e41e40fe3dd37045a67834c7f33ed40984
Viet Anh Duong <vietanhs0817@gmail.com>
2024-08-01 12:42:10 +0700

    Update config
    Signed-off-by: Viet Anh Duong <vietanhs0817@gmail.com>

--- wezterm.lua
+++ wezterm.lua
@@ -4,7 +4,7 @@ local config = wezterm.config_builder()
 
 config.font = wezterm.font 'Zed Mono'
 config.harfbuzz_features = { 'calt=0' }
-config.font_size = 15.0
+config.font_size = 14.0
 
 config.color_scheme = 'Gruvbox dark, hard (base16)'
 
