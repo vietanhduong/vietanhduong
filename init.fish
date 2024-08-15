a382798f6773a104d9883e165f2ec3b04aeea815
Viet Anh Duong <vietanhs0817@gmail.com>
2024-08-15 16:53:49 +0700

    Update config
    Signed-off-by: Viet Anh Duong <vietanhs0817@gmail.com>

--- init.fish
+++ init.fish
@@ -100,34 +100,36 @@ end
 #
 # customize fish prompt
 #
-function fish_prompt -d "custom prompt"
-    set -l last_status $status
+function fish_prompt --description 'Write out the prompt'
+    set -l last_pipestatus $pipestatus
+    set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
     set -l normal (set_color normal)
-    set -l status_color (set_color brgreen)
-    set -l cwd_color (set_color $fish_color_cwd)
+    set -q fish_color_status
     set -l vcs_color (set_color brpurple)
-    set -l prompt_status ""
-
-    # Since we display the prompt on a new line allow the directory names to be longer.
-    set -q fish_prompt_pwd_dir_length
-    or set -lx fish_prompt_pwd_dir_length 0
+    or set -g fish_color_status red
 
     # Color the prompt differently when we're root
+    set -l color_cwd $fish_color_cwd
     set -l suffix '$'
     if functions -q fish_is_root_user; and fish_is_root_user
         if set -q fish_color_cwd_root
-            set cwd_color (set_color $fish_color_cwd_root)
+            set color_cwd $fish_color_cwd_root
         end
         set suffix '#'
     end
 
-    # Color the prompt in red on error
-    if test $last_status -ne 0
-        set status_color (set_color $fish_color_error)
-        set prompt_status $status_color "[" $last_status "]" $normal
+    # Write pipestatus
+    # If the status was carried over (if no command is issued or if `set` leaves the status untouched), don't bold it.
+    set -l bold_flag --bold
+    set -q __fish_prompt_status_generation; or set -g __fish_prompt_status_generation $status_generation
+    if test $__fish_prompt_status_generation = $status_generation
+        set bold_flag
     end
+    set __fish_prompt_status_generation $status_generation
+    set -l status_color (set_color $fish_color_status)
+    set -l statusb_color (set_color $bold_flag $fish_color_status)
+    set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
 
-    echo -s (prompt_login) ' ' $cwd_color (prompt_pwd) $vcs_color (fish_vcs_prompt) $normal ' ' $prompt_status $status_color $suffix ' ' $normal
+    echo -n -s (prompt_login)' ' (set_color $color_cwd) (prompt_pwd) $vcs_color(fish_vcs_prompt) $normal " "$prompt_status " " $suffix " "
 end
-
 
