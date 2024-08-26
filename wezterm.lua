b4b1f0c9f225a77a56000e02e0c0f2e00608773f
Viet Anh Duong <vietanhs0817@gmail.com>
2024-08-26 11:31:53 +0700

    Update config
    Signed-off-by: Viet Anh Duong <vietanhs0817@gmail.com>

--- wezterm.lua
+++ wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.font = wezterm.font 'Monaco'
+config.font = wezterm.font 'Noto Sans Mono'
 config.harfbuzz_features = { 'calt=0' }
 config.font_size = 14.0
 
