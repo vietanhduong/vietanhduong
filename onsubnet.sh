b12af5d91eb17f6d0bc2c50fc83592f4aa64bdc6
Viet Anh Duong <vietanhs0817@gmail.com>
2024-08-31 21:20:22 +0700

    Fix onsubnet script
    Signed-off-by: Viet Anh Duong <vietanhs0817@gmail.com>

--- onsubnet.sh
+++ onsubnet.sh
@@ -21,7 +21,6 @@ regexp="^$(sed 's/\./\\./g' <<<"$1")"
 
 if [[ "$(uname)" == "Darwin" ]] ; then
   ifconfig | grep -F 'inet ' | grep -Fv 127.0.0. | cut -d ' ' -f 2 | grep -Eq "$regexp"
-  echo "Match"
 else
   hostname -I | tr -s " " "\012" | grep -Fv 127.0.0. | grep -Eq "$regexp"
 fi
