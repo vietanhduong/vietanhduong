542550a9234fe06e9addabc5b8ef2c88c8222337
Viet Anh Duong <vietanhs0817@gmail.com>
2025-02-20 15:30:46 +0700

    Update config
    Signed-off-by: Viet Anh Duong <vietanhs0817@gmail.com>

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -345,43 +345,20 @@ require("lazy").setup {
       require("nvim-rooter").setup()
     end,
   },
-  -- fzf support for ^p
-  {
-    "junegunn/fzf.vim",
-    dependencies = {
-      { "junegunn/fzf", dir = "~/.fzf", build = "./install --all" },
-    },
-    config = function()
-      -- stop putting a giant window over my editor
-      vim.g.fzf_layout = { down = "~20%" }
-      -- when using :Files, pass the file list through
-      --
-      --   https://github.com/jonhoo/proximity-sort
-      --
-      -- to prefer files closer to the current file.
-      function list_cmd()
-        local base = vim.fn.fnamemodify(vim.fn.expand "%", ":h:.:S")
-        if base == "." then
-          -- if there is no current file,
-          -- proximity-sort can't do its thing
-          return "fd --hidden --type file --follow"
-        else
-          return vim.fn.printf(
-            "fd --hidden --type file --follow | proximity-sort %s",
-            vim.fn.shellescape(vim.fn.expand "%")
-          )
-        end
-      end
 
-      vim.api.nvim_create_user_command("Files", function(arg)
-        vim.fn["fzf#vim#files"](
-          arg.qargs,
-          { source = list_cmd(), options = "--scheme=path --tiebreak=index" },
-          arg.bang
-        )
-      end, { bang = true, nargs = "?", complete = "dir" })
-    end,
+  {
+    "ibhagwan/fzf-lua",
+    -- optional for icon support
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    -- or if using mini.icons/mini.nvim
+    -- dependencies = { "echasnovski/mini.icons" },
+    config = function()
+      require "fzf-lua".setup {}
+      -- Use Ctrl + p to search file with Fzf
+      vim.keymap.set("n", "<C-p>", ":FzfLua files<CR>", { desc = "Fzf: Search files" })
+    end
   },
+
   -- LSP
   {
     "neovim/nvim-lspconfig",
